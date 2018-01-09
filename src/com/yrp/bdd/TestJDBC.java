0cfaff2d399c910af2d4925fc45f4d40f835952a
dvillaLG <yoaaaarp@gmail.com>
2018-01-09 18:18:35 +0100

    Adding fiew request to bdd

--- src/com/yrp/bdd/TestJDBC.java
+++ src/com/yrp/bdd/TestJDBC.java
@@ -1,17 +1,99 @@
 package com.yrp.bdd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 public class TestJDBC {
-	/* La liste qui contiendra tous les r�sultats de nos essais */
+	/* La liste qui contiendra tous les r�sultats de nos essais */
 	private List<String> messages = new ArrayList<String>();
 	
 	public List<String> executerTests(HttpServletRequest request){
-		/* Liste de tests a effectuer */
-		
-		return messages;
+		/* Chargement du driver JDBC pour MySQL */
+	    try {
+	        messages.add( "Chargement du driver..." );
+	        Class.forName( "com.mysql.jdbc.Driver" );
+	        messages.add( "Driver chargé !" );
+	    } catch ( ClassNotFoundException e ) {
+	        messages.add( "Erreur lors du chargement : le driver n'a pas été trouvé dans le classpath ! <br/>"
+	                + e.getMessage() );
+	    }
+
+	    /* Connexion à la base de données */
+	    String url = "jdbc:mysql://localhost:3306/bdd_yrp";
+	    String utilisateur = "java";
+	    String motDePasse = "yrp_ee";
+	    Connection connexion = null;
+	    Statement statement = null;
+	    ResultSet resultat = null;
+	    try {
+	        messages.add( "Connexion à la base de données..." );
+	        connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+	        messages.add( "Connexion réussie !" );
+
+	        /* Création de l'objet gérant les requêtes */
+	        statement = connexion.createStatement();
+	        messages.add( "Objet requête créé !" );
+	        try {
+	        	/* Exécution d'une requête d'écriture */
+		        int statut = statement.executeUpdate( "INSERT INTO Utilisateur (email, mot_de_passe, nom, date_inscription) VALUES ('jmarc@mail.fr', MD5('lavieestbelle78'), 'jean-marc', NOW());" );
+
+		        /* Formatage pour affichage dans la JSP finale. */
+		        messages.add( "Résultat de la requête d'insertion : " + statut + "." );
+	        } catch(SQLException e) {
+	        	messages.add( "Erreur lors de l'insertion : <br/>"
+		                + e.getMessage() );
+	        }
+	        
+	        /* Exécution d'une requête de lecture */
+	        resultat = statement.executeQuery( "SELECT id, email, mot_de_passe, nom FROM Utilisateur;" );
+	        messages.add( "Requête \"SELECT id, email, mot_de_passe, nom FROM Utilisateur;\" effectuée !" );
+	 
+	        /* Récupération des données du résultat de la requête de lecture */
+	        while ( resultat.next() ) {
+	            int idUtilisateur = resultat.getInt( "id" );
+	            String emailUtilisateur = resultat.getString( "email" );
+	            String motDePasseUtilisateur = resultat.getString( "mot_de_passe" );
+	            String nomUtilisateur = resultat.getString( "nom" );
+	            /* Formatage des données pour affichage dans la JSP finale. */
+	            messages.add( "Données retournées par la requête : id = " + idUtilisateur + ", email = " + emailUtilisateur
+	                    + ", motdepasse = "
+	                    + motDePasseUtilisateur + ", nom = " + nomUtilisateur + "." );
+	        }
+	    } catch ( SQLException e ) {
+	        messages.add( "Erreur lors de la connexion : <br/>"
+	                + e.getMessage() );
+	    } finally {
+	        messages.add( "Fermeture de l'objet ResultSet." );
+	        if ( resultat != null ) {
+	            try {
+	                resultat.close();
+	            } catch ( SQLException ignore ) {
+	            }
+	        }
+	        messages.add( "Fermeture de l'objet Statement." );
+	        if ( statement != null ) {
+	            try {
+	                statement.close();
+	            } catch ( SQLException ignore ) {
+	            }
+	        }
+	        messages.add( "Fermeture de l'objet Connection." );
+	        if ( connexion != null ) {
+	            try {
+	                connexion.close();
+	            } catch ( SQLException ignore ) {
+	            }
+	        }
+	    }
+
+	    return messages;
 	}
 }
